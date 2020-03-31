0217abec76c04c59616d850ed705d0d7f7160927
ru-obn <rufina86@bk.ru>
2020-03-31 20:04:48 +0300

    Initial commit

---
+++ src/main/java/OfferDeveloper.java
@@ -0,0 +1,13 @@
+public class OfferDeveloper {
+    public static void main(String[] args) {
+        final int offerOneCompensation = 6000;
+        final double offerOneTime = 1.35;
+        final int offerTwoCompensation = 5000;
+        final double offerTwoTime = 1.15;
+        final int offerOne = (offerOneCompensation / 8);
+        final int offerTwo = (offerTwoCompensation / 8);
+        System.out.println(offerOne);
+        System.out.println(offerTwo);
+
+    }
+}
